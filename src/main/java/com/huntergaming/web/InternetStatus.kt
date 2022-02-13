67fb6862dbce11ae541f068095945d6c47a67c9b
Jason Crosby <jason.crosby2012@gmail.com>
2022-02-13 08:10:58 -0600

    added room to use as a cache when there is no internet and migrate it to firestore when internet comes back

--- src/main/java/com/huntergaming/web/InternetStatus.kt
+++ src/main/java/com/huntergaming/web/InternetStatus.kt
@@ -2,31 +2,58 @@ package com.huntergaming.web
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-fun isConnected(@ApplicationContext context: Context): Boolean {
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+class InternetStatus @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
 
-    if (connectivityManager != null) {
-        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
-            when {
-                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                    return true
-                }
-                hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                    return true
-                }
-                hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                    return true
-                }
-            }
+    // properties
+
+    private val _internetConnectionStatus = MutableStateFlow(InternetConnectionStatus.UNKNOWN)
+    val internetConnectionStatus: StateFlow<InternetConnectionStatus> = _internetConnectionStatus
+
+    private val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        .build()
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            _internetConnectionStatus.value = InternetConnectionStatus.CONNECTED
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            _internetConnectionStatus.value = InternetConnectionStatus.DISCONNECTED
         }
     }
 
-    return false
+    // functions
+
+    fun register() {
+        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    fun unregister() {
+        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
 }
 
-// injectable
-// register and unregister
-// inject it into Activity to use call register/unregister onpause/onresume
+// enums
+
+enum class InternetConnectionStatus {
+    CONNECTED, DISCONNECTED, UNKNOWN
+}
